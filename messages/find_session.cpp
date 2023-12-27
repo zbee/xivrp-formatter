603b2446a6769edeaa123675b5b28f21e8298586
Ethan Henderson <ethan@zbee.codes>
2023-12-26 20:35:47 -0700

    Rough stub of session finding

---
+++ messages/find_session.cpp
@@ -0,0 +1,6 @@
+// XIVRP-Formatter Copyright (C) 2024 Ethan Henderson <ethan@zbee.codes>
+// Licensed under GPLv3 - Refer to the LICENSE file for the complete text
+
+#include "find_session.h"
+
+namespace session {} // namespace session
---
+++ messages/find_session.h
@@ -0,0 +1,16 @@
+// XIVRP-Formatter Copyright (C) 2024 Ethan Henderson <ethan@zbee.codes>
+// Licensed under GPLv3 - Refer to the LICENSE file for the complete text
+
+#ifndef XIVRP_FORMATTER_FIND_SESSION_H
+#define XIVRP_FORMATTER_FIND_SESSION_H
+
+namespace session {
+
+struct session {};
+struct session_list {};
+
+class find {};
+
+} // namespace session
+
+#endif // XIVRP_FORMATTER_FIND_SESSION_H
