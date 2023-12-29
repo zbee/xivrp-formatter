220a32e21877aad3b3bbbfff1e04298e18ec3998
Ethan Henderson <ethan@zbee.codes>
2023-12-29 14:41:07 -0700

    Basic stubbing of auto-finding images

---
+++ images/reshade.h
@@ -0,0 +1,13 @@
+// XIVRP-Formatter Copyright (C) 2024 Ethan Henderson <ethan@zbee.codes>
+// Licensed under GPLv3 - Refer to the LICENSE file for the complete text
+
+#ifndef XIVRP_FORMATTER_RESHADE_H
+#define XIVRP_FORMATTER_RESHADE_H
+
+namespace related_images {
+
+class reshade {};
+
+} // namespace related_images
+
+#endif // XIVRP_FORMATTER_RESHADE_H
